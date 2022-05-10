055dd1ad122c7179b975d0bc53b9ad0475b2a821
BPK <BPK@PKBHIMASHAN-Lap.luxoft.com>
2022-05-10 14:50:17 +0530

    This is my first commit messsage

---
+++ src/Bhims/A.java
@@ -0,0 +1,16 @@
+package Bhims;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry="hungry";
+		if(hungry== "yes")
+			System.out.println("Eat Something");
+		else{
+			System.out.println("Do Work");
+		}
+	}
+
+}
